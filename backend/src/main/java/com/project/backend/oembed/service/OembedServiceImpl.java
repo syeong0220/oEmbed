ded73af840d8a74074e4f48bae3f896753e0e28c
syeong0220 <hillde0220@hanmail.net>
2023-03-29 23:12:19 +0900

    commit

--- backend/src/main/java/com/project/backend/oembed/service/OembedServiceImpl.java
+++ backend/src/main/java/com/project/backend/oembed/service/OembedServiceImpl.java
@@ -12,11 +12,6 @@ import java.util.List;
 
 import org.apache.commons.validator.routines.UrlValidator;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
